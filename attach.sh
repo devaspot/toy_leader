c35ae76d3452bcd1330dd263a05a1c2d29e43c92
Namdak Tonpa <maxim@synrc.com>
2015-03-16 17:14:09 +0200

    mkpasswd

--- attach.sh
+++ attach.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NODE=${1:-"box3"}
-erl -remsh $NODE@`hostname -s` -s king attach -sname `makepasswd --char=4` -cookie synrc_cookie
+erl -remsh $NODE@`hostname -s` -s king attach -sname `mkpasswd -c 0 -s 0` -cookie synrc_cookie
